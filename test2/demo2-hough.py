88ee79151528ddc121eb4fb925b5746fbc7ca230
zhengyinloong <zhengyinloong@126.com>
2023-04-28 22:53:09 +0800

    update

--- test2/demo2-hough.py
+++ test2/demo2-hough.py
@@ -3,7 +3,7 @@
 # zhengyinloong
 # 2023/04/28 07:29
 """
-霍夫直线检测
+Hough直线检测
 """
 from mycv import *
 
@@ -32,35 +32,6 @@ if __name__=="__main__":
         # 保存
         cv.imwrite(f'edge{i+1}.jpg', edge, [cv.IMWRITE_JPEG_QUALITY, 100])
         cv.imwrite(f'hough{i+1}.jpg', hough, [cv.IMWRITE_JPEG_QUALITY, 100])
-
-
-    # img1 = cv.imread(IMG1,cv.IMREAD_GRAYSCALE)
-    # img2 = cv.imread(IMG2,cv.IMREAD_GRAYSCALE)
-    # img3 = cv.imread(IMG3,cv.IMREAD_GRAYSCALE)
-    # edge1 = edge_detecting(img1)
-    # edge2 = edge_detecting(img2)
-    # edge3 = edge_detecting(img3)
-    #
-    # lines1=get_lines(edge1)
-    # lines2=get_lines(edge2)
-    # lines3=get_lines(edge3)
-    #
-    # # 画线
-    # img1 = cv.imread(IMG1)
-    # img2 = cv.imread(IMG2)
-    # img3 = cv.imread(IMG3)
-    # hough1 = draw_lines(img1,lines1)
-    # hough2 = draw_lines(img2,lines2)
-    # hough3 = draw_lines(img3,lines3)
-    #
-    #
-    # # 显示
-    # cv.imshow('edge1',edge1)
-    # cv.imshow('edge2',edge2)
-    # cv.imshow('edge3',edge3)
-    # cv.imshow('hough1',hough1)
-    # cv.imshow('hough2',hough2)
-    # cv.imshow('hough3',hough3)
 
     # Wait until a key pressed
     cv.waitKey(0)
